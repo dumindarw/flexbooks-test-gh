b99f31f7588675122ed03a77992e05e9728e502a
Duminda Wanninayake <deemind@gmail.com>
2023-02-01 16:53:47 +0530

    checking pull request wf 5

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const supertest = require('supertest')
 const app = require('../index')
 
 describe('Integration Tests', () => {
-  test("root", async () => {
+  test('root', async () => {
     // const out = [{ name: 'abc', location: 'OoooO' }, { name: 'xyz', location: 'OoooO' }, { name: 'pqr', location: 'OoooO' }]
     await supertest(app).get('/').expect(200)
       .then((res) => {
@@ -10,7 +10,7 @@ describe('Integration Tests', () => {
       })
   })
 
-  test("hello", async () => {
+  test('hello', async () => {
     await supertest(app).get('/hello').expect(200)
       .then((res) => {
         expect(res.body).toStrictEqual({ message: 'Hello Githook' })
