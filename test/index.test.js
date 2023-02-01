aeade0206a5067daf366724dd176e3280618eac8
Duminda Wanninayake <deemind@gmail.com>
2023-02-01 16:47:04 +0530

    checking pull request wf 3

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const supertest = require('supertest')
 const app = require('../index')
 
 describe('Integration Tests', () => {
-  test('root', async () => {
+  test("root", async () => {
     // const out = [{ name: 'abc', location: 'OoooO' }, { name: 'xyz', location: 'OoooO' }, { name: 'pqr', location: 'OoooO' }]
     await supertest(app).get('/').expect(200)
       .then((res) => {
