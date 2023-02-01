afbcb20c0fab2a6ead9bb5d608c695c75fc4f869
Duminda Wanninayake <deemind@gmail.com>
2023-02-01 14:17:27 +0530

    checking pull request wf

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
 const express = require('express')
+const _ = require('underscore')
 
 const app = express()
 
+const stringifyObj2 = (o, location) => JSON.stringify({ name: o.name, location })
+
+const uniqueMbEntries = [{ name: 'abc' }, { name: 'xyz' }, { name: 'pqr' }]
+
 app.get('/', function (req, res) {
-  res.send({ message: 'Index' })
+  const location = 'OoooO'
+  const uniqueMbEntriesByLocation = _.map(uniqueMbEntries, (o) => stringifyObj2(o, location))
+
+  res.send({ message: uniqueMbEntriesByLocation })
 })
 
 app.get('/hello', function (req, res) {
