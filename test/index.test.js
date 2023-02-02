974d6f7964da3cceac6b7b978246562e97e9742c
Duminda Wanninayake <deemind@gmail.com>
2023-02-02 19:35:04 +0530

    Adding simple line

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const app = require('../index')
 
 describe('Integration Tests', () => {
   test('root', async () => {
-    // const out = [{ name: 'abc', location: 'OoooO' }, { name: 'xyz', location: 'OoooO' }, { name: 'pqr', location: 'OoooO' }]
+    // // const out = [{ name: 'abc', location: 'OoooO' }, { name: 'xyz', location: 'OoooO' }, { name: 'pqr', location: 'OoooO' }]
     await supertest(app).get('/').expect(200)
       .then((res) => {
         expect(res.body).toStrictEqual({ message: 'Root' })
